8d43181950824700b485e2c0fa204b1918ed7046
Alex Sinagin <a.sinyagin996@gmail.com>
2024-03-13 22:56:11 +0300

    add exe1

---
+++ exe1/Program.cs
@@ -0,0 +1,17 @@
+﻿void GetListNums(int left, int right)
+{
+     if(left == right) 
+     {
+        Console.WriteLine($"{left}");
+        return;
+     }
+    Console.Write($"{left} ");
+    GetListNums(left + 1, right); 
+   
+
+}
+Console.Write("Введите натуральное число число M: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число число N: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+GetListNums(num, num1);
