f666c49931ac4cb78d88e3b6d29cd6a458c9d032
Alex Sinagin <a.sinyagin996@gmail.com>
2024-03-13 23:42:48 +0300

    add exe2

---
+++ exe2/Program.cs
@@ -0,0 +1,17 @@
+﻿void Akkerman(int m, int n)
+{
+    if(m == 0) return n + 1;
+    if(m > 0 && n == 0) return Akkerman(m - 1, n = 0);
+    if(m > 0, n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    return
+}
+
+
+
+
+
+Console.Write("Введите натуральное число число M: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число число N: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Akkerman(num, num1);
